f3f8859a194172377e56daefe1ba4702b595f4a9
mohittygithub <smartfriendmohit@gmail.com>
2021-01-24 18:15:44 +0530

    created a small app to get random daily quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./app.css";
+// import axios from "axios";
+import { quotesData } from "./quotes.data";
 
-function App() {
+// advice api url = https://api.adviceslip.com/advice
+// famous quotes api url = https://type.fit/api/quotes
+
+const App = () => {
+  //   const [advice, setAdvice] = useState("Click the button to get an advice");
+  const [quote, setQuote] = useState({});
+  const [loading, setLoading] = useState(false);
+  //   const [disable, setDisable] = useState(false);
+
+  //   const fetchAdvice = async () => {
+  //     setLoading(true);
+  //     const response = await axios.get(`https://api.adviceslip.com/advice`);
+  //     const { advice } = response.data.slip;
+  //     setLoading(false);
+
+  //     setAdvice(advice);
+  //     fetchQuotes();
+  //   };
+  const fetchQuotes = () => {
+    // const response = await axios.get("https://type.fit/api/quotes");
+    // console.log(response.data);
+    setLoading(true);
+    const randomQuote =
+      quotesData[Math.floor(Math.random() * quotesData.length)];
+    // console.log(randomQuote);
+    setLoading(false);
+    setQuote({ quote: randomQuote.text, author: randomQuote.author });
+  };
+
+  useEffect(() => {
+    //   fetchAdvice();
+    fetchQuotes();
+  }, []);
+
+  if (!loading) {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{quote.quote}</h1>
+          <p className="author">-{quote.author}</p>
+          <button
+            className="button"
+            onClick={fetchQuotes}
+            disabled={loading && true}
+          >
+            Generate Quote
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">Loading advice...</h1>
+        <button
+          className="button"
+          onClick={fetchQuotes}
+          disabled={loading && true}
         >
-          Learn React
-        </a>
-      </header>
+          Generate Quote
+        </button>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
